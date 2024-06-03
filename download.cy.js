f3abb9013c36cd49bac0d234b88f3715bdcaef36
Nicolas Ribeiro <164014161+Koreste@users.noreply.github.com>
2024-06-03 10:38:38 +0200

    Update download.cy.js

--- download.cy.js
+++ download.cy.js
@@ -1,41 +1,40 @@
-describe("File upload and download tests", () => {
-    beforeEach(() => {
-      cy.visit("https://filebin.net/");
-    });
-  
-    it("Upload file and download it in Zip format", () => {
-      cy.get("#fileField").attachFile("fileToUpload.jpg");
-      cy.contains("It contains 1 uploaded file").should("be.visible");
-      cy.contains("Download files").click();
-      cy.contains("Zip")
-        .invoke("attr", "href")
-        .then((downloadLink) => {
-          const absulteLink = "https://filebin.net/" + downloadLink;
-          cy.log(absulteLink);
-          cy.downloadFile(
-            absulteLink,
-            "mydownloads/zipFiles",
-            "downloadedFromCypress.zip"
-          );
-          cy.readFile("mydownloads/zipFiles/downloadedFromCypress.zip");
-        });
-    });
-  
-    it("Upload file and download it in Tar format", () => {
-      cy.get("#fileField").attachFile("fileToUpload.jpg");
-      cy.contains("It contains 1 uploaded file").should("be.visible");
-      cy.contains("Download files").click();
-      cy.contains("Tar")
-        .invoke("attr", "href")
-        .then((downloadLink) => {
-          const absulteLink = "https://filebin.net/" + downloadLink;
-          cy.log(downloadLink);
-          cy.downloadFile(
-            absulteLink,
-            "mydownloads/tarFiles",
-            "downloadedFromCypress.tar"
-          );
+describe('Uploader',()=>{
+    const imagePath = 'Bordeaux.jpg';
+
+    it.skip('Quête 4 Cypress',()=>{
+        cy.visit('https://filebin.net/') 
+        cy.get('#fileField').attachFile(imagePath);
+        cy.get('[sorttable_customkey="Bordeaux.jpg"]').should('be.visible').and('contains.text',imagePath)
+        cy.get(':nth-child(8) > .link-primary').should('be.visible')
+        cy.contains("Tar")
+      .invoke("attr", "href")
+      .then((downloadLink) => {
+        const absulteLink = "https://filebin.net/" + downloadLink ;
+        cy.log(downloadLink);
+        cy.downloadFile(
+          absulteLink,
+          "mydownloads/tarFiles",
+          "downloadedFromCypress.tar")
           cy.readFile("mydownloads/tarFiles/downloadedFromCypress.tar");
-        });
-    });
-  });
+
+        })
+    })
+    it('Quête 4 Cypress',()=>{
+        cy.visit('https://filebin.net/') 
+        cy.get('#fileField').attachFile(imagePath);
+        cy.get('[sorttable_customkey="Bordeaux.jpg"]').should('be.visible').and('contains.text',imagePath)
+        cy.get(':nth-child(8) > .link-primary').should('be.visible')
+        cy.contains("Zip")
+      .invoke("attr", "href")
+      .then((downloadLink) => {
+        const absulteLink = "https://filebin.net/" + downloadLink ;
+        cy.log(downloadLink);
+        cy.downloadFile(
+          absulteLink,
+          "mydownloads/ZipFiles",
+          "downloadedFromCypress.zip")
+          cy.readFile("mydownloads/ZipFiles/downloadedFromCypress.zip");
+
+})
+    })
+})
